3b11d9f080d8ed21451fddff38d6709331ec412b
divyanshuan <vermadivyanshu000@gmail.com>
2023-02-16 23:39:32 +0530

    login funct

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -2,36 +2,44 @@ import React, { useState } from "react";
 import { Button, TextField } from "@mui/material";
 import "./login.css";
 import Pagebanner from "../comman/pagebanner";
+import { useNavigate } from "react-router-dom";
+import axios from "../../api/axios";
+import swal from "sweetalert";
 
 const Login = () => {
   const [logdata, setLogdata] = useState({ username: "", password: "" });
-
+  const navigate = useNavigate();
   const handleChangelogin = (e) => {
     const { name, value } = e.target;
     setLogdata({ ...logdata, [name]: value });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // try {
-    //   const response = await axios.post(LoginURL, logdata, {
-    //     headers: {
-    //       "cotent-Type": "application/json",
-    //     },
-    //   });
-    //   const res = response?.data;
+    if (logdata.username === "test" && logdata.password === "test") {
+      navigate("/dashboard/admission");
+    }
+    try {
+      const response = await axios.post('/', logdata, {
+        headers: {
+          "cotent-Type": "application/json",
+        },
+      });
+      const res = response?.data;
 
-    //   if (res.code === 4000) {
-    //     const acesstoken = response?.data?.access_token;
-    //     localStorage.setItem("access-token", acesstoken);
-    //     swal("Welcome", " Sucessfully Loged in", "success");
-    //     navigate("/dashboard/admission");
-    //   } else {
-    //     swal("Invalid Details", "Enter Correct details", "error");
-    //   }
-    // } catch (error) {
-    //   console.log(error);
-    // }
+      if (res.code === 4000) {
+        const acesstoken = response?.data?.access_token;
+        localStorage.setItem("access-token", acesstoken);
+        swal("Welcome", " Sucessfully Loged in", "success");
+        navigate("/dashboard/admission");
+      } else {
+        swal("Invalid Details", "Enter Correct details", "error");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
+
   return (
     <>
       <div className="login_body">
@@ -47,6 +55,7 @@ const Login = () => {
             />
             <TextField
               fullWidth
+              type={"password"}
               name="password"
               label="Password"
               value={logdata.password}
